43762bbd8f2645d3da06d82b155c45cc27f75240
徐国琦 <3183656478@qq.com>
2020-12-27 19:35:58 +0800

    徐国琦作业

---
+++ day10/徐国琦/徐国琦代码/app/src/main/java/com/example/day11/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.day11;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.baidu.mapapi.search.sug.OnGetSuggestionResultListener;
+import com.baidu.mapapi.search.sug.SuggestionResult;
+import com.baidu.mapapi.search.sug.SuggestionSearch;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+}
